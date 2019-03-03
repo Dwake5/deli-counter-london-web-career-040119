bfc52ab3584bea588ced40a9e42bbaef114770d5
Danny Wakeling <wakeling101@hotmail.com>
2019-03-03 14:47:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+  string = "The line is currently:"
+  array.each_with_index do |value, index|
+    string += " #{index.to_i+1}. #{value}"
+end  
+
+
+def take_a_number(array, string)
+end
